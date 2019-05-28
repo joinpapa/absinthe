fa449d2277999c06532a9343b12f433e17189c04
Marko Požega <mpozega94@gmail.com>
2019-05-28 20:37:07 +0200

    Update resolution helpers with lastest helper from v.1.5.0-alpha.4

--- lib/absinthe/resolution/helpers.ex
+++ lib/absinthe/resolution/helpers.ex
@@ -16,9 +16,27 @@ defmodule Absinthe.Resolution.Helpers do
   This is a helper function for using the `Absinthe.Middleware.Async`.
 
   Forbidden in mutation fields. (TODO: actually enforce this)
+
+  ## Options
+     - `:timeout` default: `30_000`. The maximum timeout to wait for running
+     the task.
+
+  ## Example
+
+  Using the `Absinthe.Resolution.Helpers.async/1` helper function:
+  ```elixir
+  field :time_consuming, :thing do
+    resolve fn _, _, _ ->
+      async(fn ->
+        {:ok, long_time_consuming_function()}
+      end)
+    end
+  end
+  ```
   """
   @spec async((() -> term)) :: {:middleware, Middleware.Async, term}
-  @spec async((() -> term), Keyword.t()) :: {:middleware, Middleware.Async, term}
+  @spec async((() -> term), opts :: [{:timeout, pos_integer}]) ::
+          {:middleware, Middleware.Async, term}
   def async(fun, opts \\ []) do
     {:middleware, Middleware.Async, {fun, opts}}
   end
@@ -28,7 +46,12 @@ defmodule Absinthe.Resolution.Helpers do
 
   Helper function for creating `Absinthe.Middleware.Batch`
 
-  # Example
+  ## Options
+    - `:timeout` default: `5_000`. The maximum timeout to wait for running
+    a batch.
+
+  ## Example
+
   Raw usage:
   ```elixir
   object :post do
@@ -49,13 +72,13 @@ defmodule Absinthe.Resolution.Helpers do
   ```
   """
   @spec batch(Middleware.Batch.batch_fun(), term, Middleware.Batch.post_batch_fun()) ::
-          {:plugin, Middleware.Batch, term}
+          {:middleware, Middleware.Batch, term}
   @spec batch(
           Middleware.Batch.batch_fun(),
           term,
           Middleware.Batch.post_batch_fun(),
-          opts :: Keyword.t()
-        ) :: {:plugin, Middleware.Batch, term}
+          opts :: [{:timeout, pos_integer}]
+        ) :: {:middleware, Middleware.Batch, term}
   def batch(batch_fun, batch_data, post_batch_fun, opts \\ []) do
     batch_config = {batch_fun, batch_data, post_batch_fun, opts}
     {:middleware, Middleware.Batch, batch_config}
@@ -83,12 +106,12 @@ defmodule Absinthe.Resolution.Helpers do
       resolve fn shipment, _, %{context: %{loader: loader}} ->
         loader
         |> Dataloader.load(SourceName, :automatic_reports, shipment)
-        |> Dataloader.load(SourceName, :manaul_reports, shipment)
+        |> Dataloader.load(SourceName, :manual_reports, shipment)
         |> on_load(fn loader ->
           reports =
             loader
             |> Dataloader.get(SourceName, :automatic_reports, shipment)
-            |> Enum.concat(Dataloader.load(loader, SourceName, :manaul_reports, shipment))
+            |> Enum.concat(Dataloader.get(loader, SourceName, :manual_reports, shipment))
             |> Enum.sort_by(&reported_at/1)
           {:ok, reports}
         end)
@@ -165,6 +188,13 @@ defmodule Absinthe.Resolution.Helpers do
       field :organization, :organization do
         resolve dataloader(Accounts, :organization, use_parent: false)
       end
+
+      field(:account_active, non_null(:boolean), resolve: dataloader(
+          Accounts, :account, callback: fn account, _parent, _args ->
+            {:ok, account.active}
+          end
+        )
+      )
     end
     ```
 
@@ -196,6 +226,10 @@ defmodule Absinthe.Resolution.Helpers do
     - `:args` default: `%{}`. Any arguments you want to always pass into the
     `Dataloader.load/4` call. Resolver arguments are merged into this value and,
     in the event of a conflict, the resolver arguments win.
+    - `:callback` default: `default_callback/3`. Callback that is run with result
+    of dataloader. It receives the result as the first argument, and the parent
+    and args as second and third. Can be used to e.g. compute fields on the return
+    value of the loader. Should return an ok or error tuple.
     - `:use_parent` default: `true`. This option affects whether or not the `dataloader/2`
     helper will use any pre-existing value on the parent. IE if you return
     `%{author: %User{...}}` from a blog post the helper will by default simply use
@@ -254,9 +288,14 @@ defmodule Absinthe.Resolution.Helpers do
       |> use_parent(source, resource, parent, args, opts)
       |> Dataloader.load(source, {resource, args}, parent)
       |> on_load(fn loader ->
-        result = Dataloader.get(loader, source, {resource, args}, parent)
-        {:ok, result}
+        callback = Keyword.get(opts, :callback, &default_callback/3)
+
+        loader
+        |> Dataloader.get(source, {resource, args}, parent)
+        |> callback.(parent, args)
       end)
     end
+
+    defp default_callback(result, _parent, _args), do: {:ok, result}
   end
 end
